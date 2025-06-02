9d131dbc28b523ee7a2d5437038c53c9a32de53a
pje1122 <jeongeun1122@naver.com>
2025-06-03 02:01:44 +0900

    조리대 개선

--- Assets/Script/stoveCollision.cs
+++ Assets/Script/stoveCollision.cs
@@ -2,16 +2,30 @@ using UnityEngine;
 
 public class StoveCollision : MonoBehaviour
 {
-    private void OnTriggerStay(Collider other)
+    private bool isPlayerInTrigger = false;
+
+    private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            // Shift 키를 누른 "순간"만 감지
-            if (Input.GetKeyDown(KeyCode.LeftShift))
-            {
-                //Debug.Log("조리대앞에서 Shift 키가 눌렸습니다!");
-                StoveManager.Instance.ToOven("meat");
-            }
+            isPlayerInTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInTrigger = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (isPlayerInTrigger && Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            //Debug.Log("조리대 앞에서 Shift 키가 눌렸습니다!");
+            StoveManager.Instance.ToOven("meat");
         }
     }
 }
